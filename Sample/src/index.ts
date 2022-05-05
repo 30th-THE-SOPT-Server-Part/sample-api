b9ef014dea2020308304cd4d6e49a04f48179ceb
jokj624 <jokj624@gmail.com>
2022-05-05 22:22:50 +0900

    Feat: error handling

--- Sample/src/index.ts
+++ Sample/src/index.ts
@@ -3,6 +3,9 @@ import "./config/env"
 import './config/keys';
 import express from 'express';
 import { RegisterRoutes } from "./routes/routes";
+import { ValidateError } from "tsoa";
+import { CustomError } from "./interfaces/common/CustomError";
+import { errorHandling } from "./utils/error";
 
 const app = express();
 
@@ -14,5 +17,52 @@ try {
 } catch (e) {
     express.request.next?.(e);
 }
+
+app.use(function errorHandler(
+    err: any,
+    req: express.Request,
+    res: express.Response | any,
+    next: express.NextFunction
+) {
+    let reason;
+    let location;
+    let status: number;
+
+    if (err instanceof ValidateError) {
+        const { status: localStatus, name, fields } = err;
+
+        reason = name;
+        const fieldKey = Object.keys(fields)[0];
+
+        location = fields[fieldKey].message;
+        status = localStatus;
+    } else {
+        const customError: CustomError = errorHandling(err);
+        const { status: localStatus, error } = customError;
+
+        reason = error.reason;
+        location = error.location;
+        status = localStatus;
+    }
+
+    if (status >= 500) {
+        res.status(status).json({
+            status,
+            success: false,
+            message: "Internal Server Error"
+        });
+    } else {
+        res.status(status).json({
+            status,
+            success: false,
+            message: {
+                reason: reason,
+                location: location
+            }
+        });
+    }
+
+    next();
+})
 
 export = app;
